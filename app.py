3750cd08d3530d432c242628f14ccfbea0e34526
Dennis Vink <dennisvink@binx.io>
2023-06-07 18:14:32 +0200

    Cleaned up Flask app

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ openai.api_type = 'azure'
 openai.api_version = "2023-03-15-preview"
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='', static_folder='static')
 
 def ask_model(context):
     prompt = f"""{context}"""
@@ -29,103 +29,74 @@ def ask_model(context):
 # Lander HTML code is from WhatThePug.com. You'll want to replace it with something simpler I guess :)
 @app.route('/', methods=['GET', 'POST'])
 def landing_page():
-    lander = """
-<!DOCTYPE html>
-<html class="min-h-screen" lang="en">
+    lander = """<!DOCTYPE html>
+<html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>My AI Website</title>
-    <meta name="description" content="My Amazing AI Tool" />
-    <meta name="robots" content="noindex,nofollow">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Xebia Sample AI Tool</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/Nunito.css">
+    <link rel="stylesheet" href="assets/css/aitool.css">
   </head>
-  <body>
-    <textarea class="modelprompt" id='prompt' placeholder="Context and prompt" rows=8 cols=70></textarea>
-    <br />
-    <button class="modelbtn" id='ask'>Asketh</button> <span style='color: #ccc'>&#11013; &#65039; Click here to query!</span>
-    <br />
-    <br />
-    </td>
-    </tr>
-    </table>
-    <table>
-      <tr>
-        <td colspan=2>
-          <textarea class="modelanswer" id='answer' placeholder="The model's answer will appear here..." rows=8 cols=101 readonly></textarea>
-          <button id="copy-button">Copy answer</button>
-        </td>
-      </tr>
-    </table>
-    <table class='difference'>
-      <div id='diff'></div>
-    </table>
-    <script type="text/javascript">
-      function constructQueryString() {
-        let name = encodeURIComponent(document.getElementById('name').value);
-        let job = encodeURIComponent(document.getElementById('authority').value);
-        let speaker = encodeURIComponent(document.getElementById('speaker').value);
-        let context = encodeURIComponent(document.getElementById('context').value);
-        let queryString = `https://whatthepug.com/?name=${name}&job=${job}&speaker=${speaker}&context=${context}`;
-        console.log('Your AI URL:', queryString);
-      }
-
-      function createLabelAndElement(labelText, element, elementName) {
-        const label = document.createElement('label');
-        label.setAttribute('for', elementName);
-        label.textContent = labelText + ': ';
-
-        element.setAttribute('name', elementName);
-
-        const container = document.createElement('span');
-        container.appendChild(label);
-        container.appendChild(element);
-
-        return container;
-      }
-
-      function showDiv(elem) {
-        var selectedValue = elem.value;
-        var modelDiv = document.querySelector('.model');
-        var intro = document.querySelector('.introduction');
-        var modelprompt = document.getElementById('prompt');
-        if (selectedValue === '--') {
-          modelDiv.style.display = 'none';
-          intro.style.display = 'block';
-        } else {
-          intro.style.display = 'none';
-          let stylebox = document.querySelector('.style');
-          let difference = document.querySelector('.difference');
-          if(selectedValue == '1') {
-            stylebox.style.display = 'block';
-            difference.style.display = 'block';
-            modelprompt.placeholder = 'Write your text here, and select your preferred style. Spelling Pug will fix your spelling, style and grammar errors and rewrite your text in your preferred style. Trust in the power of the model!';
-          } else {
-            stylebox.style.display = 'none';
-            difference.style.display = 'none';
-            if(selectedValue == '2') {
-              modelprompt.placeholder = 'Write your text here. Explainer Pug will summarise the key points and the intention of the author in plain English. Trust in the power of the model!';
-            } else {
-              modelprompt.placeholder = 'Write your text here. Sentiment Pug will tell you whether this is OK to send in a business context, and will tell you what the tone and sentiment of the message is. Trust in the power of the model!';
-            }
-          }
-          let modelimg = document.querySelector('.modelimg');
-          modelimg.src = 'https://storage.googleapis.com/whatthepug/whatthepug-300x300_' + selectedValue + '.png';
-          modelDiv.style.display = 'block';
-        }
-      }
-
-    </script>
+  <body id="page-top">
+    <div class="overlay"></div>
+    <div class="spinner">
+      <div class="lds-grid"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+      <div class="text-center thinking"><span>Thinking...</span></div>
+    </div>
+    <div id="wrapper">
+      <div class="d-flex flex-column" id="content-wrapper">
+        <div id="content">
+          <div class="container-fluid">
+            <div class="d-sm-flex justify-content-between align-items-center mb-4">
+              <h3 class="text-dark mb-0"></h3>
+            </div>
+            <section class="py-4 py-xl-5">
+              <div class="card mb-5">
+                <div class="card-body p-sm-5">
+                  <h2 class="text-center mb-4">Xebia AI Tool</h2>
+                  <div class="mb-3">
+                    <textarea class="form-control" id="input" name="message" rows="8" placeholder="Input to LLM"></textarea>
+                    <br />
+                    <textarea class="form-control" id="output" name="message" rows="8" placeholder="Output of LLM"></textarea>
+                  </div>
+                  <div><button class="btn btn-primary d-block w-100 querybtn" id='querybtn' type="submit">Query LLM</button></div>
+                </div>
+              </div>
+            </section>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/theme.js"></script>
     <script>
-      document.getElementById('ask').addEventListener('click', async () => {
-        const prompt = document.getElementById('prompt').value;
+      function showSpinner() {
+        var overlay = document.querySelector('.overlay');
+        var spinner = document.querySelector('.spinner');
 
-        const answerTextarea = document.getElementById('answer');
+        overlay.style.display = 'block';
+        spinner.style.display = 'block';
+      }
+
+      function hideSpinner() {
+        var overlay = document.querySelector('.overlay');
+        var spinner = document.querySelector('.spinner');
+
+        overlay.style.display = 'none';
+        spinner.style.display = 'none';
+      }
+
+      document.getElementById('querybtn').addEventListener('click', async () => {
+        const prompt = document.getElementById('input').value;
+        const answerTextarea = document.getElementById('output');
         if (prompt.trim() === '') {
           alert('Please type a prompt.');
           return;
         }
-
-        let response = await fetch('https://yourwebsiteurl.example.com/', {
+        showSpinner();
+        let response = await fetch(window.location.origin + "/", {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -135,27 +106,10 @@ def landing_page():
         if (response.ok) {
           const data = await response.json();
           answerTextarea.value = data.answer.trim();
-          var styles = document.getElementsByName('style');
-          var selector = document.getElementById('model-selector');
-          var selectedValue = selector.value;
-          if(selectedValue == '1') {
-            var wikEdDiff = new WikEdDiff();
-            var orig = document.getElementById('prompt').value;
-            var _new = data.answer.trim();
-            var diffHtml = wikEdDiff.diff(orig, _new);
-            var div = document.getElementById('diff');
-            div.innerHTML = diffHtml;
-          }
         } else {
           answerTextarea.value = 'I am sleeping right now. Please try again later.';
         }
-      });
-    </script>
-    <script>
-      const copyButton = document.getElementById('copy-button');
-      const textarea = document.getElementById('answer');
-      copyButton.addEventListener('click', (event) => {
-        textarea.select();document.execCommand('copy'); window.getSelection().removeAllRanges();
+        hideSpinner();
       });
     </script>
   </body>
